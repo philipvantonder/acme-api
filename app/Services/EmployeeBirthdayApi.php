5ba06c3b29f1ae0355e66c7d1987d883d5c71a21
philip <pvantonder157@gmail.com>
2020-08-18 22:11:39 +0200

    Added error checking

--- app/Services/EmployeeBirthdayApi.php
+++ app/Services/EmployeeBirthdayApi.php
@@ -9,7 +9,14 @@ class EmployeeBirthdayApi extends \App\Services\Employee {
     public function executeInternal($name) 
     {
 
-		if (date("m-d", strtotime($this->employee->dateOfBirth)) == date("m-d")) {
+		$is_leap_year = date("L");
+
+		$employeee_birth_date = date("m-d", strtotime($this->employee->dateOfBirth));
+		if (!$is_leap_year && date("m-d", strtotime($this->employee->dateOfBirth)) == "02-29") {
+			$employeee_birth_date = date("m-d", strtotime("{$this->employee->dateOfBirth} -1 day"));
+		}
+
+		if ($employeee_birth_date == date("m-d")) {
 			\Mail::to(config('services.acme_soft.email'))->send(new EmployeeBirthday($name));
 		}
 		
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Services\EmployeeBirthdayApi;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\RequestException;
 
 function view_arr($arr) { echo "<pre>"; print_r($arr); echo "</pre>"; }
 
@@ -24,25 +26,33 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::get('/birthdayWishes', function(Request $request) {
 
-	$client = new \GuzzleHttp\Client();
+	try {
 
-	$request = $client->get(config('services.acme_soft.api_url') . 'Employees', ['verify' => false]);
+		$client = new \GuzzleHttp\Client();
+		
+		view_arr(config('services.acme_soft.api_url'));
 
-	$response = $request ? $request->getBody()->getContents() : null;
+		$employeeRequest = $client->get(config('services.acme_soft.api_url') . 'Employees', ['verify' => false]);
+		
+		$BirthdayWishExclusionsRequest = $client->get(config('services.acme_soft.api_url') . 'BirthdayWishExclusions', ['verify' => false]);
 	
-	$status = $request ? $request->getStatusCode() : 500;
-
-	if ($response && $status === 200 && $response !== 'null') {
-
-		$employee_arr = json_decode($request->getBody());
-
+		$BirthdayWishExclusionsRequest = json_decode($BirthdayWishExclusionsRequest->getBody());
+		
+		$employee_arr = json_decode($employeeRequest->getBody());
+		
 		foreach ($employee_arr as $employee) {
-
+			
+			if (!empty($BirthdayWishExclusionsRequest) && in_array($employee->id, $BirthdayWishExclusionsRequest)) {
+				continue;
+			}
+			
 			$birthday = new EmployeeBirthdayApi($employee);
 			$birthday->execute();
-
+			
 		}
 
+	} catch (\GuzzleHttp\Exception\RequestException $e) {
+		Log::error("Birthday API have failed");
 	}
-
+	
 });
